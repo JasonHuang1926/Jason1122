6c5113984880826611079332d3ac5f6b07aec8c6
jasonHuang <jason.huang@tpinformation.com.tw>
2016-11-22 14:53:08 +0800

    First IOS APP

---
+++ iOS/LoginFlow/LoginViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace Jason.iOS
+{
+	public partial class LoginViewController : UIViewController
+	{
+		public LoginViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.	
+
+			btnLogin.TouchUpInside += (sender, e) => {
+				Console.WriteLine("hello !!");
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
