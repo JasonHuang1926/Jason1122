e594d298b26fb61a959dbe57f3ebbbc3672280ad
jasonHuang <jason.huang@tpinformation.com.tw>
2016-11-22 18:10:13 +0800

    iOS Layout

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,20 +7,31 @@ namespace Jason.Droid
 	[Activity(Label = "Jason", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		//int count = 1;
+		private EditText _txtAccount;
+
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+			// iOS Xcode Custom Class
+			// View - Controller Binding
+			SetContentView(Resource.Layout.loginflow_loginview);
+
+			// View's Element - Controller's UI Control Binding
+			_txtAccount = FindViewById<EditText>(Resource.Id.loginflow_loginview_txtaccount);
+
+			var txtPassword =FindViewById<EditText>(Resource.Id.loginflow_loginview_txtpassword);
+
+			var btnLogin = FindViewById<Button>(Resource.Id.loginflow_loginview_btnlogin);
+			btnLogin.Click += (sender, e) =>{ };
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			//button.Click += delegate { button.Text = $"{count++} clicks!"; };
 		}
 	}
 }
